86b7ace442aa9792d00e25e1f40f7379f155d68c
tomszociepa <tom.ociepa@gmail.com>
2023-07-05 13:09:07 +0200

    queue subscription RabbitMQ

--- src/main/java/com/mango/receiver/ReceiverMessageController.java
+++ src/main/java/com/mango/receiver/ReceiverMessageController.java
@@ -1,8 +1,11 @@
 package com.mango.receiver;
 
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.time.LocalDateTime;
 
 
 @RestController
@@ -17,11 +20,15 @@ public class ReceiverMessageController {
     @GetMapping("/message")
     public String receiverMessage() {
         Object message = rabbitTemplate.receiveAndConvert("stau");
-        if(message != null){
+        if (message != null) {
             return "udało się odczytać wiadomość: " + message.toString();
-        }else {
+        } else {
             return "Nie ma wiadomości do odczytu";
         }
+    }
 
+    @RabbitListener(queues = "stau")
+    public void listenerMessage(String message){
+        System.out.println(LocalDateTime.now() + " "+ message);
     }
 }
